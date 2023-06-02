475631875c1c52bd53f734b7473761695c853ce0
Vansh Awasthi <75345433+VanshAwasthi@users.noreply.github.com>
2023-06-02 14:45:35 +0530

    Adding Login Component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Navbar from "./components/Navbar";
 import Home from "./components/Home";
 import About from "./components/About";
 import Alert from "./components/Alert";
+import Login from "./components/Login";
+import Signup from "./components/Signup";
 import NoteState from "./context/notes/NoteState";
 
 function App() {
@@ -17,6 +19,8 @@ function App() {
             <Routes>
               <Route path="/" element={<Home />} />
               <Route path="/about" element={<About />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/signup" element={<Signup/>} />
             </Routes>
           </div>
         </BrowserRouter>
